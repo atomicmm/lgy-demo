dd53d072ed2157244b3c503deda1001c0a24aa02
Atomic <atomicmm@gmail.com>
2015-08-23 01:05:58 +0800

    1scroll内只能嵌套一个linear. 2.当需要按钮左右各占一半时,要设置weightsum属性 3.要下半部分固定上半部分滚动时,也是设置这个属性.注意是继承父类大小还是wrap_content根据内容大小.

---
+++ src/com/example/lgy/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.lgy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Atomic on 2015/8/23.
+ */
+public class DetailActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+    }
+}
